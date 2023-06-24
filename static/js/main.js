3853056264a57a655bee2f9942effb2f7753535c
KidMongoose <kidmongoose@gmail.com>
2023-06-24 18:53:32 +0700

    CSS and HTML for music template

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,10 @@
 $(() => {
-    
+    $('#playlist-button').on('click', (e) => {
+         $('.jukebox-container .songs').hide()
+         $('.jukebox-container .playlists').show()
+    })
+    $('#song-button').on('click', (e) => {
+        $('.jukebox-container .playlists').hide()
+        $('.jukebox-container .songs').show()
+    })
 })
