9bafdec50b8cbdbcbf19c9d89d566177a029c264
KidMongoose <kidmongoose@gmail.com>
2023-06-25 15:26:39 +0700

    Method to get album cover art and null functions

--- models.py
+++ models.py
@@ -2,10 +2,17 @@ from mongoframes import *
 from pymongo import MongoClient
 from dotenv import load_dotenv
 from datetime import datetime
+from tinytag import TinyTag
+from PIL import Image
 import os
+import secrets
+import lyricsgenius
+import shutil
+import io
 
 load_dotenv('.env')
 Frame._client = MongoClient(os.getenv('DATABASE_URI'))
+genius = lyricsgenius.Genius(os.getenv('LYRICSGENIUS_API_KEY'))
 
 class User(Frame):
     _fields = {
@@ -17,8 +24,23 @@ class User(Frame):
       'playlist'
     }
 
+    def register(user):
+        return User(**user).insert()
+
+    def deactivate():
+        pass
+
+    def delete():
+        pass
+
+    def edit():
+        pass
+
+
+
 class Song(SubFrame):
     _fields = {
+        'song_id',
         'artist',
         'song_title',
         'duration',
@@ -29,6 +51,30 @@ class Song(SubFrame):
         'song_id'
     }
 
+    def add():
+        pass
+
+    def get_cover_art(song):
+        tag = TinyTag.get(f'uploads/songs/{song}', image=True)
+        album_cover = tag.get_image()
+        im = Image.open(io.BytesIO(album_cover))
+        im.save(f'uploads/images/{song}.png')
+        return tag
+
+
+
+    def delete():
+        pass
+
+    def edit():
+        pass
+
+    def retrieve_track():
+        pass
+
+    def retrieve_tracks():
+        pass
+
 class Playlist(SubFrame):
     _fields = {
         'name',
@@ -38,3 +84,17 @@ class Playlist(SubFrame):
         'likes',
         'fans'
     }
+    def create():
+        pass
+
+    def delete():
+        pass
+
+    def edit():
+        pass
+
+    def retrieve_all():
+        pass
+
+    def retrieve():
+        pass
