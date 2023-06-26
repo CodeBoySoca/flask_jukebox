92b7b4d893bf27183c825fe63a4dc4c89dc4e44f
KidMongoose <kidmongoose@gmail.com>
2023-06-26 09:59:59 +0700

    CSS and js for search and lyrics overlay

--- static/js/main.js
+++ static/js/main.js
@@ -11,4 +11,27 @@ $(() => {
     $('.jukebox-container .songs .artist').on('click', (e) => {
         $('#footer-content ul, #footer-content p').remove()
     })
+
+    $('#track-container button').on('click', (e) => {
+        $('.lyrics-overlay').show()
+        let h2 = document.createElement('h2')
+        let p = document.createElement('p')
+        h2.setAttribute('id', 'title')
+        h2.innerHTML ='lyrics here'
+
+    })
+
+
+    $('#search').on('click', (e) => {
+        e.preventDefault()
+        $('.search-overlay').show()
+
+    })
+
+    $('.close').on('click', (e) => {
+        //e.preventDefault()
+        $('.search-overlay, .lyrics-overlay').hide()
+
+    })
+
 })
